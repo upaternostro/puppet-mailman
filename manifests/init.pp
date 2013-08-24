fbf68bfa925ee9f66b35c9c2e81b175a88133530
Nic Waller <code@nicwaller.com>
2013-08-24 12:45:24 -0700

    Added validation and a bit of cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,8 @@
 #
 # TODO: allow uninstallation with "ensure => absnet"
 #
+# Designed to work with Mailman >= 2.1.5, mostly because of where files are located.
+#
 # === Parameters
 # Try to keep defaults similar to Mailman defaults. Exceptions will be clearly noted.
 #
@@ -22,17 +24,15 @@
 #
 # === Examples
 #
-#  class { mailman:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
+#  include mailman
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Nic Waller <code@nicwaller.com>
 #
 # === Copyright
 #
-# Copyright 2013 Your name here, unless otherwise noted.
+# Copyright 2013 Nic Waller, unless otherwise noted.
 #
 class mailman (
 	$activate_qrunners = false,
@@ -45,27 +45,57 @@ class mailman (
 	$mailman_site_list = 'mailman',
 	$mta = 'manual',
 
-        $default_send_reminders = 'True',
+        $default_send_reminders = true,
         $default_archive_private = '0',
         $default_max_message_size = '40', # in KB
         $default_msg_footer = '""',
         $default_subscribe_policy = '1',
         $default_private_roster = '1',
         $default_generic_nonmember_action = '1',
-        $default_forward_auto_discards = 'True',
-        $default_require_explicit_destination = 'True',
+        $default_forward_auto_discards = true,
+        $default_require_explicit_destination = true,
         $default_max_num_recipients = '10',
 
-	$virtual_host_overview = 'True',
+	$virtual_host_overview = true,
 
 	$smtp_max_rcpts = '500',
 
+	$prefix = '/usr/lib/mailman',
 	$log_dir = '/var/log/mailman',
 	$lock_dir = '/var/lock/mailman',
 	$pid_dir = '/var/run/mailman',
 	$var_prefix = '/var/lib/mailman',
-	$queue_dir = "/var/spool/mailman",
+	$queue_dir = '/var/spool/mailman',
 ) {
+	$langs = ['ar','ca','cs','da','de','en','es','et','eu','fi','fr','gl','he',
+		'hr','hu','ia','it','ja','ko','lt','nl','no','pl','pt','pt_BR','ro',
+		'ru','sk','sl','sr','sv','tr','uk','vi','zh_CN','zh_TW']
+	validate_bool($activate_qrunners)
+	validate_re($language, $langs)
+	validate_re($mailman_site_list, '[-+_.=a-z0-9]*')
+	validate_re($mta, ['manual', 'postfix'])
+	validate_bool($default_send_reminders)
+	validate_re($default_archive_private, [0,1])
+	validate_re($default_max_message_size, '[0-9]*')
+	validate_re($default_msg_footer, ['^""".*$', '".*"'])
+	validate_re($default_subscribe_policy, [0,1,2,3])
+	validate_re($default_private_roster, [0,1,2])
+	validate_re($default_generic_nonmember_action, [0,1,2,3])
+	validate_bool($default_forward_auto_discards)
+	validate_bool($default_require_explicit_destination)
+	validate_re($default_max_num_recipients, '[0-9]*')
+	validate_bool($virtual_host_overview)
+	validate_re($smtp_max_rcpts, '[0-9]*')
+
+	$exec_prefix = $prefix
+	$config_dir = '/etc/mailman'
+	$spam_dir = "${var_prefix}/spam"
+	$wrapper_dir = "${exec_prefix}/mail"
+	$bin_dir = "${prefix}/bin"
+	$scripts_dir = "${prefix}/scripts"
+	$template_dir = "${prefix}/templates"
+	$messages_dir = "${prefix}/messages"
+
 	# Originally I wanted to use native Python path joins exactly the same
 	# as is done in Defaults.py. However, it is useful to have all of the
 	# paths fully resolved in the Puppet manifest so they can be used with
@@ -100,27 +130,19 @@ class mailman (
         $retryqueue_dir  = "${queue_dir}/retry"
         $maildir_dir     = "${queue_dir}/maildir"
 
-
-	# TODO activate_qrunners is one of "stopped" or "running", kindof
-	# TODO booleans must be input as exactly True or False, and validated
-	# TODO carefully validate input to ensure it meets the quoting/non-quoting
-	# requirements for python variables
-
-	# TODO: Either localize exec $path or state dependency on Mailman >= 2.1.5
-
 	# TODO: is there a simpler way to generate a decent password?
 	# TODO: avoid generating a password every time?
-	$site_list_pw = generate("/bin/sh", "-c", "/bin/dd bs=64 count=1 if=/dev/urandom 2> /dev/null | /usr/bin/tr -dc 'a-zA-Z0-9' | /usr/bin/fold -c10 | /usr/bin/head -n1")
+	$site_list_pw = generate("/bin/sh", "-c", "PATH=/bin:/usr/bin; dd bs=64 count=1 if=/dev/urandom 2> /dev/null | tr -dc 'a-zA-Z0-9' | fold -c16 | head -n1")
 	package { 'mailman':
 		ensure  => installed,
-	} -> file { '/usr/lib/mailman/Mailman/mm_cfg.py':
+	} -> file { "${prefix}/Mailman/mm_cfg.py":
 		content => template("${module_name}/mm_cfg.py.erb"),
 		owner   => 'root',
 		group   => 'mailman',
-		mode    => '0640',
+		mode    => '0644',
 	} -> exec { 'create_site_list':
-		command => "newlist --language=${language} --urlhost=${default_url_domain} --emailhost=${default_email_host} --quiet '${mailman_site_list}' '${mailman_site_list}@${default_email_host}' '${site_list_pw}'",
-		path    => "/usr/lib/mailman/bin",
+		command => "newlist --quiet '${mailman_site_list}' '${mailman_site_list}@${default_email_host}' '${site_list_pw}'",
+		path    => $bin_dir,
 		creates => "${list_data_dir}/${mailman_site_list}/config.pck",
 	} -> service { 'mailman':
 		ensure  => $activate_qrunners,
