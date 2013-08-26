3ef24ebb5e02bf8ff5ef97cb44c56a1390e865f2
Nic Waller <code@nicwaller.com>
2013-08-25 22:00:14 -0700

    Update documentation

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,36 @@
 # == Class: mailman::params
 #
-# Full description of class mailman here.
+# This class is used to setup reasonable defaults for the essential parameters
+# of a Mailman configuration.
+#
+# Unfortunately, Puppet manifests make it impossible to define parameters that
+# have default values which derive from other parameters to the same class.
+# The best workaround is to extract this logic into a "params" class and make
+# other classes inherit from this one to be sure that all of these variables
+# are realized. To have parameter defaults derive from other parameters more
+# than one layer deep, you need to use multiple layers of inheritance. I try
+# to keep it simple here, and only use one layer.
 #
 # === Parameters
 #
-# [*MTA*]
-#   The MTA param names a module in the Mailman/MTA dir which contains the mail
-#   server-specific functions to be executed when a list is created or removed.
+# [*prefix*]
+# The most important variable in a Mailman installation is prefix, because so
+# many other variables derive from it. Traditionally the prefix would be set
+# during compilation according to where you want the files located, but modern
+# packages all use the same FHS-compliant path.
+#
+# [*exec_prefix*]
+# Since the default for exec_prefix is to be exactly the same as prefix, I think
+# this might be a legacy holdover from older versions of Mailman.
+#
+# [*var_prefix*]
+# Changing var_prefix is the fastest way to relocate all of your site-specific
+# data including mailing list configuration and archives. Typically if you are
+# changing var_prefix, you may also want to change queue_dir in the init class.
 #
 # === Examples
 #
-#  include mailman::params
+# include mailman::params
 #
 # === Authors
 #
