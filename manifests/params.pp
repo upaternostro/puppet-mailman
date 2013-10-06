eaa6a0fc4950ceb6bfd19074d822881a1e0efcf5
Nic Waller <code@nicwaller.com>
2013-10-05 21:55:31 -0700

    Fix template_dir on Fedora 19 to resolve issue #18

--- manifests/params.pp
+++ manifests/params.pp
@@ -51,7 +51,7 @@ class mailman::params {
       $wrapper_dir   = "${exec_prefix}/mail"
       $bin_dir       = "${prefix}/bin"
       $scripts_dir   = "${prefix}/scripts"
-      if ($::operatingsystem == 'Fedora') and ($::operatingsystemversion == 19) {
+      if ($::operatingsystem == 'Fedora') and ($::operatingsystemmajrelease == 19) {
         $template_dir  = '/etc/mailman/templates'
       } else {
         $template_dir  = "${prefix}/templates"
