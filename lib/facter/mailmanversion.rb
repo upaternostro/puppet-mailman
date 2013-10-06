1e6c04479ca6cbfd6f1dd95a21a6e547663da1e9
Nic Waller <code@nicwaller.com>
2013-10-05 19:16:32 -0700

    Unquality awk path to fix #23

--- lib/facter/mailmanversion.rb
+++ lib/facter/mailmanversion.rb
@@ -1,5 +1,5 @@
 Facter.add("mailmanversion") do
   setcode do
-    Facter::Util::Resolution::exec("/usr/lib/mailman/bin/version | /bin/awk '{print $4}'")
+    Facter::Util::Resolution::exec("/usr/lib/mailman/bin/version | awk '{print $4}'")
   end
 end
